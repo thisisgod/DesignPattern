238639ad65bd5b7e04da706f5c98caee598a8e03
skyde47 <rlaud1998@naver.com>
2022-02-13 16:40:03 +0900

    DesignPattern 03 Abstract Factory 끝

---
+++ src/main/java/_03_abstract_factory/ShipInventory.java
@@ -0,0 +1,10 @@
+package _03_abstract_factory;
+
+public class ShipInventory {
+    public static void main(String[] args) {
+        ShipFactory shipFactory = new WhiteshipFactory(new WhitePartsProFactory());
+        Ship ship = shipFactory.createShip();
+        System.out.println(ship.getAnchor().getClass());
+        System.out.println(ship.getWheel().getClass());
+    }
+}
